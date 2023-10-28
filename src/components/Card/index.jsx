be9e70c9944e337398228c59592512096a3fcc89
Rodrigo Fischer <rodrigofischer4@gmail.com>
2023-10-28 17:41:22 -0300

    Feitas algumas alterações no estilo do card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,18 +1,23 @@
 import './card.css'
+import Button from '@mui/material/Button';
 
 const Card = ({ name, description, teams }) => {
     
     return (<div className='card'>
         <div className='container'>
-            <h3 className='fluxName'>{name}</h3>
-        
+            <h3 className='fluxName'>{name}</h3>        
             <div className='description'>
                 <h5>{description}</h5>
                 <h5>{teams}</h5>
             </div>
         </div>
 
-        <button>Ver Mais</button>
+        <Button variant="contained"
+         style={{ marginBottom: '30px',
+          backgroundColor: '#fe4900'
+          }}>
+            Ver Mais</Button>
+
 
     </div>
 
