24ffed68cad3637eff47d06a3d43839486b7b070
Rodrigo Fischer <rodrigofischer4@gmail.com>
2023-11-16 12:55:14 -0300

    updated card style makig it responsive

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -7,8 +7,12 @@ const Card = ({ name, description, teams }) => {
         <div className={Styles.container}>
             <h3 className={Styles.fluxName}>{name}</h3>        
             <div className={Styles.description}>
-                <h5>{description}</h5>
-                <h5>{teams}</h5>
+                <div className={Styles.information}>
+                    <h5>{description}</h5>
+                </div>
+                <div className={Styles.teams}>
+                    <h5>{teams}</h5>
+                </div>
             </div>
         </div>
 
@@ -17,10 +21,7 @@ const Card = ({ name, description, teams }) => {
           backgroundColor: '#fe4900'
           }}>
             Ver Mais</Button>
-
-
     </div>
-
     )
 }
 
