fc82a0a780808437f88fcc42853ae48ee1daba76
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2023-11-16 20:30:05 -0300

    feat: creates text editor

--- src/pages/NewFlow/NewFlow.jsx
+++ src/pages/NewFlow/NewFlow.jsx
@@ -1,19 +1,32 @@
 import { useNavigate } from 'react-router-dom'
 
 import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
-import { TextField } from '@mui/material';
-import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
-import AttachFileIcon from '@mui/icons-material/AttachFile';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import { Button, TextField } from '@mui/material';
+
+import AttachFileOutlinedIcon from '@mui/icons-material/AttachFileOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import FormatBoldIcon from '@mui/icons-material/FormatBold';
 import FormatItalicIcon from '@mui/icons-material/FormatItalic';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined';
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
+
+import { EditorContent, useEditor } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
 
 import { Header } from '../../components/Header'
 import style from './styles.module.css'
 
+
 export function NewFlow() {
   const navigate = useNavigate()
+
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+    ],
+    content: 'Insira seu texto aqui...',
+  })
 
   const handleRedirectToHome = () => {
     return navigate('/home')
@@ -28,21 +41,12 @@ export function NewFlow() {
           style={{ cursor: 'pointer' }}
         />
         <div>
-          {/* <TextField
-            label=''
-            type='email'
-            variant='standard'
-            margin='dense'
-            // value={title}
-            // onChange={(e) => handleEmailValidation(e.target.value)}
-          /> */}
            <TextField
             id="outlined-multiline-flexible"
             variant='standard'
             multiline
             maxRows={2}
             placeholder='Título do fluxo aqui...'
-            // sx={{border: 'none',"& fieldset": { border: 'none' },}}
             InputProps={{
               disableUnderline: true,
             }}
@@ -51,7 +55,6 @@ export function NewFlow() {
               height: '2.875rem',
               margin: '2.375rem 0 0.875rem',
             }}
-            // className={styles['MuiTextField-root']}
           />
           <TextField
             id="outlined-multiline-flexible"
@@ -59,7 +62,6 @@ export function NewFlow() {
             multiline
             maxRows={2}
             placeholder='Nome da(s) equipe(s)'
-            // sx={{border: 'none',"& fieldset": { border: 'none' },}}
             InputProps={{
               disableUnderline: true,
             }}
@@ -69,19 +71,61 @@ export function NewFlow() {
               marginBottom: '0.875rem'
             }}
           />
-            <div className={style.iconsContainer}>
-              <ImageOutlinedIcon style={{color: '#21409A', background: '#21409A15', padding: '0.5rem 1.125rem', borderRadius: '4px'}} />
-              <AttachFileIcon style={{background: '#09E85E15', padding: '0.5rem 1.125rem', borderRadius: '4px'}} />
-              <FormatListBulletedIcon />
-              <FormatBoldIcon />
-              <FormatItalicIcon />
-              <FormatUnderlinedIcon />
-            </div>
-            <div>
-              Editor aqui
-            </div>
+            {editor && (
+              <div className={style.iconsContainer}>
+                <div>
+                  <ImageOutlinedIcon style={{ color: '#21409A', background: '#21409A15', padding: '0.5rem 1.125rem', borderRadius: '4px' }} />
+                  <AttachFileOutlinedIcon
+                    style={{
+                      background: '#09E85E15',
+                      padding: '0.5rem 1.125rem', 
+                      borderRadius: '4px'
+                    }} 
+                  />
+                  <FormatListBulletedIcon 
+                    onClick={() => editor.chain().focus().toggleBulletList().run()}
+                    style={{ transform: 'scale(1.2)' }}
+                  />
+                  <FormatBoldIcon 
+                    onClick={() => editor.chain().focus().toggleBold().run()}
+                    style={{ transform: 'scale(1.2)' }}
+                  />
+                  <FormatItalicIcon
+                    onClick={() => editor.chain().focus().toggleItalic().run()}
+                    style={{ transform: 'scale(1.2)' }}
+                  />
+                  <FormatUnderlinedIcon
+                    onClick={() => editor.chain().focus().toggleStrike().run()}
+                    style={{ transform: 'scale(1.2)' }}
+                  />
+                </div>
+                <div>
+                  <DeleteOutlineOutlinedIcon variant="outlined" style={{ width: '30px', height: '30px', color: '#FE4900' }} />
+                  <Button 
+                    variant="outlined" 
+                    style={{
+                      color: '#FE4900',                 
+                      border: '1px solid #FE4900'
+                    }} 
+                  >
+                    Salvar fluxo
+                  </Button>
+                  <Button 
+                    variant="outlined" 
+                    style={{
+                      color: '#FE4900',                 
+                      border: '1px solid #FE4900'
+                    }} 
+                  >
+                    Adicionar fluxo
+                  </Button>
+                </div>
+              </div>
+            )}
+            <EditorContent editor={editor}  className={style.editorContainer}/>
         </div>
       </div>
     </>
   )
 }
+
