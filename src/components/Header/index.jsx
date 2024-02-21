a995f94d427c812a301043a6c6a4e6234a4dd384
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2024-02-20 21:59:01 -0300

    feat: adds redirect to home when clicking on header logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,13 +3,21 @@ import Stack from '@mui/material/Stack';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import styles from './styles.module.css'
 import logoLetraU from '../../assets/images/Logo-u.svg'
+import { useNavigate } from 'react-router-dom';
 
 export function Header() {
+  const navigate = useNavigate()
+
+  const handleRedirectToHome = () => {
+    navigate('/home')
+  }
+
   return ( 
     <div className={styles.homePage}>
       <img 
         src={logoLetraU}
         alt="Logo simplificado da Unisinos, contendo apenas a letra U"
+        onClick={handleRedirectToHome}
       />       
       <Stack>
         <IconButton aria-label="delete">
